5657663f0f2583b7e85bac8cf4f91c6d4da76e06
Christopher Howard <derhowiedesigns@gmail.com>
2016-04-09 12:13:53 -0500

    readme.md update + additional test added to test.js

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 import test from 'ava';
 import fn from './';
 
-test('title', t => {
+test('should remove symbols/punctuation', t => {
 	t.is(fn('unicorns()!.!!!{}[]%^&$*#&'), 'unicorns');
+});
+
+test('expected a string', t => {
+	t.throws(() => {
+		fn(342);
+	}, 'Expected a string');
 });
